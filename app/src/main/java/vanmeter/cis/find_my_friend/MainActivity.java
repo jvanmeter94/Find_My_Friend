e636b5d327f597112ce4c41052a36658ee2ca2aa
jvanmeter94 <jvanmeter94@gmail.com>
2015-12-04 18:53:12 -0500

    Activity

--- app/src/main/java/vanmeter/cis/find_my_friend/MainActivity.java
+++ app/src/main/java/vanmeter/cis/find_my_friend/MainActivity.java
@@ -1,11 +1,12 @@
 package vanmeter.cis.find_my_friend;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
